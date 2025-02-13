8ecec75449135458b7aba65c4833ec225b5b4c8c
AhmedAmami <amamihmed@gmail.com>
2025-02-13 15:47:06 +0100

    fix lint errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,22 +1,13 @@
 """
 Simple tests
 """
-
 from django.test import TestCase
-
 from app import calc
-
-
 class CalcTests(TestCase):
-
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         res = calc.add(3, 8)
-
         self.assertEqual(res, 11)
-
-
-
     def test_substract_numbers(self):
         """Test that values are subtracted and returned"""
         res = calc.subtract(10, 15)
