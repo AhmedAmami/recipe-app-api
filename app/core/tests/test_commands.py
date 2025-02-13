63985debfbd541afb790aadcd5b923344b3c5f91
AhmedAmami <amamihmed@gmail.com>
2025-02-13 21:33:08 +0100

    Configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,10 +10,9 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check') 
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """ Test commands"""
-
     def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for db when db is available"""
         patched_check.return_value = True
@@ -25,7 +24,7 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for db when db is not available"""
-        patched_check.side_effect = [OperationalError] * 2 + \
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
